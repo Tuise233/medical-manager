45b1a75763d6a8aabe6e209d86b26fbb9ce12bc8
拉布拉马 <2039203690@qq.com>
2024-11-08 11:13:10 +0800

    feat: 实现公告模块

--- src/api/modules/announce.ts
+++ src/api/modules/announce.ts
@@ -2,8 +2,16 @@ import http from '@/api';
 import { ResPage, ResultData } from '../interface';
 import { Announcement } from '../interface/announce';
 
-export async function getValidAnnouncement(pageSize: number, pageNum: number, searchValue: string) {
+export async function getValidAnnouncement(pageSize: number, pageNum: number, searchValue: string, valid: boolean = true) {
     return http.get<ResPage<Announcement>>('announcement/valid', {
-        pageSize, pageNum, searchValue
+        pageSize, pageNum, searchValue, valid
     });
+}
+
+export async function updateAnnouncement(target: Announcement) {
+    return http.post<ResultData<Announcement>>(`announcement/update/${target.id}`, target, { loading: true });
+}
+
+export async function deleteAnnouncement(id: number) {
+    return http.post<ResultData<Announcement>>(`announcement/delete/${id}`, { loading: true });
 }
--- src/views/announce/manager/index.vue
+++ src/views/announce/manager/index.vue
@@ -1,10 +1,151 @@
-<script lang="ts">
+<script lang="ts" setup>
+import { Announcement } from '@/api/interface/announce';
+import { deleteAnnouncement, getValidAnnouncement, updateAnnouncement } from '@/api/modules/announce';
+import { ref, Ref, onMounted, nextTick } from 'vue';
+import PageDivider from '../../../components/PageDivider/index.vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+
+const data: Ref<Announcement[]> = ref<Announcement[]>([]);
+const pageSize = ref(15);
+const pageNum = ref(1);
+const total = ref(0);
+const searchValue = ref('');
+const showDialog = ref(false);
+const editItem: Ref<Announcement | undefined> = ref<Announcement | undefined>();
+
+onMounted(async () => {
+    await nextTick();
+    queryAnnouncement();
+});
+
+function handlePageChange(curPage: number) {
+    pageNum.value = curPage;
+    queryAnnouncement();
+}
+
+async function queryAnnouncement() {
+    const results = await getValidAnnouncement(pageSize.value, pageNum.value, searchValue.value, false);
+    data.value = results.data.list;
+    pageNum.value = results.data.pageNum;
+    pageSize.value = results.data.pageSize;
+    total.value = results.data.total;
+}
+
+function handlePageSizeChange(size: number) {
+    pageSize.value = size;
+}
+
+function edit(item: Announcement) {
+    editItem.value = item;
+    showDialog.value = true;
+};
+
+function saveItem() {
+    if(!editItem.value) return;
+    ElMessageBox.confirm(
+        '你确定要保存这条公告的数据吗?',
+        '保存公告',
+        {
+            confirmButtonText: '保存',
+            cancelButtonText: '取消',
+            type: 'warning'
+        }
+    ).then(async () => {
+        if(!editItem.value) return;
+        const result = await updateAnnouncement(editItem.value);
+        if(Number(result.code) === 200) {
+            ElMessage({
+                message: '保存成功',
+                type: 'success'
+            });
+            showDialog.value = false;
+            editItem.value = undefined;
+            await nextTick();
+            queryAnnouncement();
+        }
+    });
+}
+
+function deleteItem() {
+    if(!editItem.value) return;
+    ElMessageBox.confirm(
+        '你确定要删除这条公告吗?',
+        '删除公告',
+        {
+            confirmButtonText: '删除',
+            cancelButtonText: '取消',
+            type: 'warning'
+        }
+    ).then(async () => {
+        if(!editItem.value) return;
+        const result = await deleteAnnouncement(editItem.value.id);
+        if(Number(result.code) === 200) {
+            ElMessage.success('你成功删除这条公告');
+            showDialog.value = false;
+            editItem.value = undefined;
+            await nextTick();
+            queryAnnouncement();
+        }
+    });
+}
 
 </script>
 
 <template>
-    <div class="app-announce-manager">
+    <div class="app-page">
+        <div class="app-header">
+            <el-button type="primary">创建新公告</el-button>
+            <PageDivider @page-size-change="handlePageSizeChange" />
+        </div>
 
+        <div class="app-content">
+            <el-table :data="data" stripe>
+                <el-table-column prop="id" label="编号" width="100"></el-table-column>
+                <el-table-column prop="title" label="标题"></el-table-column>
+                <el-table-column prop="description" label="内容"></el-table-column>
+                <el-table-column label="发布时间">
+                    <template #default="scope">
+                        <span>{{ new Date(scope.row.create_date).toLocaleString() }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="有效到期时间">
+                    <template #default="scope">
+                        <span>{{ new Date(scope.row.expire_date).toLocaleString() }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button type="primary" @click="edit(scope.row)">编辑</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize" @change="handlePageChange" />
+        </div>
+
+        <el-dialog v-model="showDialog" width="500" title="编辑公告">
+            <div class="app-edit" v-if="editItem">
+                <div>
+                    <span>标题</span>
+                    <el-input type="text" placeholder="请输入标题" v-model="editItem.title"></el-input>
+                </div>
+
+                <div>
+                    <span>公告内容</span>
+                    <el-input type="textarea" :rows="5" placeholder="请输入公告内容" v-model="editItem.description"></el-input>
+                </div>
+
+                <div>
+                    <span>有效到期时间</span>
+                    <el-date-picker v-model="editItem.expire_date" type="datetime" placeholder="请选择有效到期时间"></el-date-picker>
+                </div>
+
+                <div class="app-button">
+                    <el-button type="danger" @click="deleteItem">删除</el-button>
+                    <el-button type="primary" @click="saveItem">保存</el-button>
+                </div>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <style src="./style.scss" scoped />
