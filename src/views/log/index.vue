42246f0c60156c10d37d7a1efd13fe1448506cac
拉布拉马 <2039203690@qq.com>
2024-11-12 02:24:14 +0800

    feat: 优化日志操作

--- src/views/log/index.vue
+++ src/views/log/index.vue
@@ -29,7 +29,7 @@ async function queryLogs() {
         startDate: dateRange.value?.[0],
         endDate: dateRange.value?.[1]
     };
-    
+
     const results = await getLogPage(params);
     data.value = results.data.list;
     pageNum.value = results.data.pageNum;
@@ -56,29 +56,25 @@ function resetSearch() {
             <div class="app-search">
                 <div class="search-item">
                     <span>日志内容</span>
-                    <el-input type="text" v-model="searchValue" placeholder="请输入日志内容"/>
+                    <el-input type="text" v-model="searchValue" placeholder="请输入日志内容" />
                 </div>
                 <div class="search-item">
                     <span>时间范围</span>
-                    <el-date-picker
-                        v-model="dateRange"
-                        type="daterange"
-                        range-separator="至"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期"
-                    />
+                    <el-date-picker v-model="dateRange" type="daterange" range-separator="至" start-placeholder="开始日期"
+                        end-placeholder="结束日期" />
                 </div>
                 <div class="search-buttons">
                     <el-button type="primary" @click="queryLogs">搜索</el-button>
                     <el-button @click="resetSearch">重置</el-button>
                 </div>
             </div>
-            <PageDivider @page-size-change="handlePageSizeChange"/>
+            <PageDivider @page-size-change="handlePageSizeChange" />
         </div>
 
         <div class="app-content">
             <el-table :data="data" stripe>
                 <el-table-column prop="id" label="ID" width="80"></el-table-column>
+                <el-table-column prop="user_id" label="管理员ID" width="110"></el-table-column>
                 <el-table-column prop="action" label="日志内容"></el-table-column>
                 <el-table-column label="创建时间" width="180">
                     <template #default="scope">
@@ -87,13 +83,8 @@ function resetSearch() {
                 </el-table-column>
             </el-table>
 
-            <el-pagination 
-                background 
-                layout="prev, pager, next" 
-                :total="total" 
-                :page-size="pageSize" 
-                @current-change="handlePageChange" 
-            />
+            <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
+                @current-change="handlePageChange" />
         </div>
     </div>
 </template>
